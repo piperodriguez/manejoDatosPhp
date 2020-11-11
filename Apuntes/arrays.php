472c14f30c140632ab8996246ed61abf505f5adc
frodriguez <vargasjuan367@gmail.com>
2020-11-11 12:24:30 -0500

    arrays sencillos y complejos

--- Apuntes/arrays.php
+++ Apuntes/arrays.php
@@ -4,5 +4,62 @@
  * ARRAY SIMPLE
  */
 
-$conocimientos = ['js', 'nodejs', 10 => 'php', 'larvel'];
-print_r($conocimientos);
+$conocimientosOne = ['js', 'nodejs', 10 => 'php', 'laravel'];
+
+
+//print_r($conocimientos);
+/**
+ * ARRAY COMPLEJO
+ * 
+ * segun el proge
+ */
+$conocimientos = [
+    'FrontEnd' => 'js',
+    'BackEnd' => 'nodejs',
+    'framework' => 'laravel',
+];
+//print_r($conocimientosBase);
+
+/**
+ * iterando el array
+ * con un ciclo
+ */
+ /*foreach ($conocimientos as $key => $value) {
+    echo "$key: $value <br>";  
+ }*/
+/**
+ * iterando el array
+ * con funciona nativa de php
+ * 
+ */
+ echo "<pre>";
+ function iterar($arr, $key)
+ {
+     echo strtoupper($arr).": $key <br>";
+ }
+
+array_walk($conocimientos, 'iterar');
+
+/**
+ * array_key_exist -> verifica llave
+ * in_array -> nose
+ * array_keys -> get keys
+ * array_values -> get values
+ */
+
+ print_r(array_values($conocimientos));
+
+
+
+
+
+
+
+
+
+
+$conocimientosBase = [
+    'FrontEnd' => ['js', 'jquery', 'bootstrap', 'vue'],
+    'BackEnd' => ['nodejs', 'php'],
+    'framework' => ['laravel', 'yii2', 'express',]
+];
